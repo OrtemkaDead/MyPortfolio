eeab3211b867a6384255a4fb17f14193d228ff49
Hokage <56502279+OrtemkaDead@users.noreply.github.com>
2021-04-07 16:42:28 +0300

    beta 0.3

--- js/script.js
+++ js/script.js
@@ -11,3 +11,18 @@ for (let anchor of anchors) {
       })
    })
 }
+toggleThemeBtn = document.getElementById('theme-button')
+
+if (!localStorage.theme) localStorage.theme = 'dark'
+document.body.className = localStorage.theme
+toggleThemeBtn.innerHTML = document.body.classList.contains('light') ? 'W<span class="text-hide">hite</span>' : 'D<span class="text-hide">ark</span>'
+
+toggleThemeBtn.onclick = () => {
+   //TODO: сделать нормальное переключение + чтобы значение сохронялось
+   // document.getElementById('imgRadial').classList.toggle('radial')
+   // document.title = document.body.classList.contains('light') ? `ACode &#127773;` : `ACode &#127770;`
+
+   document.body.classList.toggle('light')
+   toggleThemeBtn.innerHTML = document.body.classList.contains('light') ? 'W<span class="text-hide">hite</span>' : 'D<span class="text-hide">ark</span>'
+   localStorage.theme = document.body.className || 'dark'
+}
