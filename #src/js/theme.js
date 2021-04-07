13ae883005a1f097f9deb57f6dbd7d915c89138f
OrtemkaDead <temakravchenko2004@gmail.com>
2021-04-07 17:44:26 +0300

    Changing themes 0.2

--- #src/js/theme.js
+++ #src/js/theme.js
@@ -2,18 +2,14 @@ toggleThemeBtn = document.getElementById('theme-button')
 
 if (!localStorage.theme) localStorage.theme = 'dark'
 document.body.className = localStorage.theme
-toggleThemeBtn.innerText = document.body.classList.contains('light') ? 'W' : 'D'
+toggleThemeBtn.innerHTML = document.body.classList.contains('light') ? 'W<span class="text-hide">hite</span>' : 'D<span class="text-hide">ark</span>'
 
 toggleThemeBtn.onclick = () => {
+   //TODO: сделать нормальное переключение + чтобы значение сохронялось
+   // document.getElementById('imgRadial').classList.toggle('radial')
+   // document.title = document.body.classList.contains('light') ? `ACode &#127773;` : `ACode &#127770;`
+
    document.body.classList.toggle('light')
-   toggleThemeBtn.innerText = document.body.classList.contains('light') ? 'W' : 'D'
+   toggleThemeBtn.innerHTML = document.body.classList.contains('light') ? 'W<span class="text-hide">hite</span>' : 'D<span class="text-hide">ark</span>'
    localStorage.theme = document.body.className || 'dark'
-}
-
-toggleThemeBtn.onmouseover = () => {
-   console.log('cursor')
-}
-
-toggleThemeBtn.onmouseout = () => {
-   console.log('no cursor')
 }
